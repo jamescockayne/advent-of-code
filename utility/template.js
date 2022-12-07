fc8a674a2bef268d7ff74c367282a6f280e4098a
James Cockayne <jamescockayne1@gmail.com>
2022-12-07 19:25:47 +0000

    Add template

---
+++ utility/template.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const rawInput = fs.readFileSync('./input.txt').toString().split("\n");
+
+const part1 = () => {
+    console.log(rawInput[0]);
+}
+
+const part2 = () => {
+
+}
+
+console.log(part1());
